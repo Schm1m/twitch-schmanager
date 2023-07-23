34300722b8ad1324bc8162ab03dce992ac6e4b58
schm1m <nix@nix.com>
2023-07-24 00:43:08 +0200

    implemented new command error handling

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,6 @@
 import { say, onlinechannels } from "../auth.js"
+import { predefError } from "../functions.js"
+import { error } from "console";
 
 export const data = {
     name: 'say',
@@ -10,6 +12,7 @@ export async function execute(channel, msg) {
     let forward = msg.split(' ').slice(2).join(' ')
     if (!onlinechannels.includes(toChannel)) {
         await say(channel, `could not send a message to channel: ${toChannel}`)
+        error(predefError("invalid target"))
     }
     await say(toChannel, forward)
 }
